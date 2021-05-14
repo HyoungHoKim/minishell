7e02292bc66c58dff97b630a5304726068bae0bd
seushin <seushin@student.42seoul.kr>
2021-05-14 16:18:51 +0900

    update header

--- minishell.h
+++ minishell.h
@@ -7,28 +7,21 @@
 #define	STDIN		0
 #define MAXSIZE		1024
 
-typedef struct		s_token
-{
-    char            *command;
-    char            *option;
-    char			*data;
-}                   t_token;
-
 typedef struct		s_cline
 {
 	char			flag;
 	char			*com_line;
-	t_token			token;
+	char			**token;
 	struct	s_cline *next;
 }					t_cline;
 
 extern char         **g_envp;
 
 void				show_prompt();
-char		    	**copy_envp(char **envs);
+char				**copy_envp(char **envs);
 void				handle_signal(int signo);
 int					get_line(char **line);
-int             	get_chr_pos(char *line, char c);
+int					get_chr_pos(char *line, char c);
 void				parser(t_cline **head, char *line);
 t_cline		        *cline_new(char *com_line, char flag);
 void				cline_add_back(t_cline **lst, t_cline *new);
