30d87f1ebae9189c8184cf6bf77b47334972b23a
hyoukim <dkdemfp2@naver.com>
2021-05-18 13:02:23 +0900

    exec_command 비교 문자열 길이 변경 명령어 길이 + 1

--- exec_command.c
+++ exec_command.c
@@ -6,7 +6,7 @@
 /*   By: hyoukim <hyoukim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/16 21:07:51 by hyoukim           #+#    #+#             */
-/*   Updated: 2021/05/17 19:16:56 by hyoukim          ###   ########.fr       */
+/*   Updated: 2021/05/18 13:00:22 by hyoukim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,13 @@ int			exec_command(char **token)
 	
 	if (token[0] == NULL)
 		return (SUCCESS);
-	len = ft_strlen(token[0]);
-	if (ft_strncmp(token[0], "echo", len) == 0)
+	if (ft_strncmp(token[0], "echo", 5) == 0)
 		ft_echo(token);
-	if (ft_strncmp(token[0], "pwd", len) == 0)
+	if (ft_strncmp(token[0], "pwd", 4) == 0)
 		ft_pwd();
-	if (ft_strncmp(token[0], "env", len) == 0)
+	if (ft_strncmp(token[0], "env", 4) == 0)
 		ft_env(token);
-	if (ft_strncmp(token[0], "cd", len) == 0)
+	if (ft_strncmp(token[0], "cd", 3) == 0)
 		ft_cd(token);
 	return (SUCCESS);
 }
