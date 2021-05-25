685f713ef6306de3eb56bcc5aa7eac322787ad42
seushin <seushin@student.42seoul.kr>
2021-05-25 18:42:28 +0900

    fix: signal로 프로그램 종료시 exit 값 수정

--- src/utils_sig.c
+++ src/utils_sig.c
@@ -6,7 +6,7 @@
 /*   By: seushin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/21 18:40:13 by seushin           #+#    #+#             */
-/*   Updated: 2021/05/25 17:46:37 by seushin          ###   ########.fr       */
+/*   Updated: 2021/05/25 18:42:03 by seushin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,15 @@ void		handle_signal(int signo)
 	if (signo == SIGINT)
 	{
 		if (pid == -1)
+		{
 			show_new_line();
+			g_state.errno = 1;
+		}
 		else
+		{
 			ft_putstr_fd("\n", STDOUT_FILENO);
+			g_state.errno = status + 128;
+		}
 	}
 	else if (signo == SIGQUIT)
 	{
@@ -36,7 +42,7 @@ void		handle_signal(int signo)
 			ft_putstr_fd("Quit: ", STDOUT_FILENO);
 			ft_putnbr_fd(status, STDOUT_FILENO);
 			ft_putchar_fd('\n', STDOUT_FILENO);
-			g_state.errno = status;
+			g_state.errno = status + 128;
 		}
 	}
 }
