cb2db0bcbf8d47768952ac691be9c28b4786ca13
seushin <seushin@student.42seoul.kr>
2021-05-25 17:12:29 +0900

    fix: SIGQUIT를 받아 자식프로세스 status를 출력하도록 수정

--- utils_sig.c
+++ utils_sig.c
@@ -6,7 +6,7 @@
 /*   By: seushin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/21 18:40:13 by seushin           #+#    #+#             */
-/*   Updated: 2021/05/23 17:56:01 by seushin          ###   ########.fr       */
+/*   Updated: 2021/05/25 17:11:47 by seushin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,10 @@ void		handle_signal(int signo)
 	pid_t	pid;
 	int		status;
 
-	// waitpid에서 첫번째 인자를 -1로 두고, WNOHANG을 옵션으로 두면 임의의 프로세스 중 어떤 프로세스라도
-	// 종료되거나 멈추지 않았더라도 즉시 리턴하라는 의미
-	pid = waitpid(-1, &status, WNOHANG);
+	status = 0;
+	pid = waitpid(-1, &status, 0);
 	if (signo == SIGINT)
 	{
-		// waitpid의 리턴값이 -1이면 자식 프로세스가 존재하지 않거나, 인터럽트시 반환
 		if (pid == -1)
 			show_new_line();
 		else
@@ -34,9 +32,12 @@ void		handle_signal(int signo)
 	}
 	else if (signo == SIGQUIT)
 	{
-		if (pid == -1)
-			show_new_line();
-		else
-			ft_putstr_fd("Quit...\n", STDOUT_FILENO);
+		if (pid != -1)
+		{
+			ft_putstr_fd("Quit: ", STDOUT_FILENO);
+			ft_putnbr_fd(status, STDOUT_FILENO);
+			ft_putchar_fd('\n', STDOUT_FILENO);
+			g_state.errno = status;
+		}
 	}
 }
