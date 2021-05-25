9cdf0e11f6b19f3c813b52a56295682dbb3049b9
seushin <seushin@student.42seoul.kr>
2021-05-25 17:52:48 +0900

    add: err_msg의 err_type 앞에 ": "를 출력하도록 추가

--- src/err_msg.c
+++ src/err_msg.c
@@ -6,16 +6,18 @@
 /*   By: seushin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/25 15:37:42 by seushin           #+#    #+#             */
-/*   Updated: 2021/05/25 17:47:23 by seushin          ###   ########.fr       */
+/*   Updated: 2021/05/25 17:52:39 by seushin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include <unistd.h>
 
 int		err_msg(char *err_type, char *comment, int errno)
 {
 	ft_putstr_fd("bash: ", STDERR_FILENO);
 	ft_putstr_fd(err_type, STDERR_FILENO);
+	ft_putstr_fd(": ", STDERR_FILENO);
 	ft_putstr_fd(comment, STDERR_FILENO);
 	ft_putchar_fd('\n', STDERR_FILENO);
 	g_state.errno = errno;
