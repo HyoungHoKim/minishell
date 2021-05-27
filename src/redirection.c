64f312e4998829b6d1fa469831de0b3c5a8a4e48
seushin <seushin@student.42seoul.kr>
2021-05-27 14:33:35 +0900

    fix: normed

--- src/redirection.c
+++ src/redirection.c
@@ -6,62 +6,80 @@
 /*   By: seushin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 13:13:36 by seushin           #+#    #+#             */
-/*   Updated: 2021/05/26 14:59:45 by seushin          ###   ########.fr       */
+/*   Updated: 2021/05/27 14:14:38 by seushin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include <sys/fcntl.h>
 
-int			get_redir_count(char **token)
+static char	**create_new_token(char **token, int *len)
 {
-	int		res;
+	char	**res;
 	int		i;
 
-	res = 0;
+	*len = 0;
 	i = 0;
 	while (token[i])
 	{
 		if (token[i][0] == '>' || token[i][0] == '<')
 		{
-			res++;
+			(*len)++;
 			i++;
 		}
 		i++;
 	}
+	if (*len == 0)
+		return (NULL);
+	*len = token_size(token) - *len * 2;
+	if (!(res = ft_calloc(sizeof(char *), *len + 1)))
+		return (NULL);
 	return (res);
 }
 
-int			cut_off_redirection(t_cmd *cmd)
+static int	cut_off_redirection(t_cmd *cmd)
 {
 	char	**res;
+	int		len;
 	int		i;
 	int		j;
-	int		len;
 
-	if (get_redir_count(cmd->token) == 0)
-		return (SUCCESS);
-	len = token_size(cmd->token) - (get_redir_count(cmd->token) * 2);
-	if (!(res = ft_calloc(sizeof(char *), len + 1)))
+	if (!(res = create_new_token(cmd->token, &len)))
 		return (FAILURE);
 	i = 0;
 	j = 0;
 	while (i < len)
 	{
-		if (cmd->token[j] && (cmd->token[j][0] == '>' || cmd->token[j][0] == '<'))
+		if (cmd->token[j] &&
+				(cmd->token[j][0] == '>' || cmd->token[j][0] == '<'))
 		{
 			free(cmd->token[j++]);
 			free(cmd->token[j]);
 		}
 		else
-		{
-			res[i] = cmd->token[j];
-			i++;
-		}
+			res[i++] = cmd->token[j];
 		j++;
 	}
 	free(cmd->token);
 	cmd->token = res;
+	return (SUCCESS);
+}
+
+static int	dup_fd_to_stdio(t_cmd *cmd, int fd[2])
+{
+	if (fd[0] == -1 || fd[1] == -1)
+		return (FAILURE);
+	if (fd[0])
+	{
+		dup2(fd[0], STDIN_FILENO);
+		close(fd[0]);
+	}
+	if (fd[1])
+	{
+		dup2(fd[1], STDOUT_FILENO);
+		close(fd[1]);
+	}
+	cut_off_redirection(cmd);
 	return (SUCCESS);
 }
 
@@ -82,25 +100,12 @@ int			find_redirection(t_cmd *cmd)
 		{
 			if (token[i][1] == '>')
 				fd[1] = open(token[i + 1],
-						(O_CREAT | O_APPEND | O_WRONLY) ,0644);
+						(O_CREAT | O_APPEND | O_WRONLY), 0644);
 			else
 				fd[1] = open(token[i + 1],
-						(O_CREAT | O_TRUNC | O_WRONLY) ,0644);
+						(O_CREAT | O_TRUNC | O_WRONLY), 0644);
 		}
 		i++;
 	}
-	if (fd[0] == -1 || fd[1] == -1)
-		return (FAILURE);
-	if (fd[0])
-	{
-		dup2(fd[0], STDIN_FILENO);
-		close(fd[0]);
-	}
-	if (fd[1])
-	{
-		dup2(fd[1], STDOUT_FILENO);
-		close(fd[1]);
-	}
-	cut_off_redirection(cmd);
-	return (SUCCESS);
+	return (dup_fd_to_stdio(cmd, fd));
 }
