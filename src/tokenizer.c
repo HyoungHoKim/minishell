410daea6a58fef4ce2a68ce604b9368b54adad25
seushin <seushin@student.42seoul.kr>
2021-05-27 17:07:45 +0900

    fix: 파싱 에러
    - hi"$HOME"과 "$HOME"hi 를 정상적으로 출력하지 못하는 에러 수정
    - 달러사인으로 환경변수 치환시 값을 잘못 가져오는 에러 수정

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -6,23 +6,39 @@
 /*   By: hyoukim <hyoukim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/13 14:27:59 by hyoukim           #+#    #+#             */
-/*   Updated: 2021/05/26 12:49:01 by seushin          ###   ########.fr       */
+/*   Updated: 2021/05/27 16:57:43 by seushin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void			add_token(t_cmd *cmd, t_parse *parse, int *buf_i)
+static void			add_token(t_cmd *cmd, t_parse *parse, int *buf_i, int flag)
 {
 	char			*single_token;
+	char			*tmp;
+	static char		*save;
 
 	*buf_i = 0;
 	if (ft_strlen(parse->buf) == 0)
 		return ;
+	if (flag)
+	{
+		save = expand_var(parse->buf);
+		ft_memset(parse->buf, 0, sizeof(parse->buf));
+		return ;
+	}
 	if (parse->quote == '\'')
 		single_token = ft_strdup(parse->buf);
 	else
 		single_token = expand_var(parse->buf);
+	if (save)
+	{
+		tmp = ft_strjoin(save, single_token);
+		free(save);
+		free(single_token);
+		single_token = tmp;
+		save = NULL;
+	}
 	token_push_back(&(cmd->token), single_token);
 	ft_memset(parse->buf, 0, sizeof(parse->buf));
 }
@@ -30,41 +46,42 @@ static void			add_token(t_cmd *cmd, t_parse *parse, int *buf_i)
 static int			parse_single_char(t_cmd **cmd, t_parse *parse, int *buf_i)
 {
 	if (is_set(*parse->input, "\"\'"))
+	{
 		parse->quote = *parse->input;
+		if (*buf_i != 0)
+			add_token(*cmd, parse, buf_i, 1);
+	}
 	else if (is_set(*parse->input, ";|"))
 	{
-		add_token(*cmd, parse, buf_i);
+		add_token(*cmd, parse, buf_i, 0);
 		(*cmd)->flag = get_flag(*parse->input);
 		if (!*(*cmd)->token)
 			return (FAILURE);
 		*cmd = add_cmd(*cmd);
 	}
 	else if (*parse->input == ' ')
-		add_token(*cmd, parse, buf_i);
+		add_token(*cmd, parse, buf_i, 0);
 	else if (is_set(*parse->input, "<>"))
 	{
-		add_token(*cmd, parse, buf_i);
+		add_token(*cmd, parse, buf_i, 0);
 		parse->buf[(*buf_i)++] = *parse->input;
 		if (*parse->input == '>' && *(parse->input + 1) == '>')
 			parse->buf[(*buf_i)++] = *parse->input++;
-		add_token(*cmd, parse, buf_i);
+		add_token(*cmd, parse, buf_i, 0);
 	}
 	else if (*parse->input != '\\')
 		parse->buf[(*buf_i)++] = *parse->input;
 	return (SUCCESS);
 }
 
-static void			parse_in_quote(t_parse *parse, t_cmd *cmd, int *buf_i)
+static void			parse_in_quote(t_parse *parse, int *buf_i)
 {
 	static int		bs_flag;
 
 	if (*buf_i == 0)
 		bs_flag = 0;
 	if (parse->quote == *parse->input && !bs_flag)
-	{
-		add_token(cmd, parse, buf_i);
 		parse->quote = 0;
-	}
 	else
 	{
 		if (bs_flag)
@@ -89,10 +106,10 @@ int					tokenizer(t_parse *parse, t_cmd *cmd)
 		if (parse->quote == 0)
 			err |= parse_single_char(&cmd, parse, &buf_i);
 		else
-			parse_in_quote(parse, cmd, &buf_i);
+			parse_in_quote(parse, &buf_i);
 		parse->input++;
 	}
-	add_token(cmd, parse, &buf_i);
+	add_token(cmd, parse, &buf_i, 0);
 	parse->input = head;
 	if (parse->quote || err)
 		return (FAILURE);
